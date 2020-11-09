1a7f5f3121456d1b31b12aa057ed78b43c63e628
evilAdan0s <eviladan0s@gmail.com>
2020-11-09 19:07:16 +0800

    添加2.1
    运行单一实例的代码和readme

---
+++ 第2章 基础技术/运行单一实例/single_running_instance/single_running_instance/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Diagnostics;
+
+namespace single_running_instance
+{
+    class Program
+    {
+        Boolean isAlreadyRun()
+        {
+            bool isRun;
+            Process currentProcess = Process.GetCurrentProcess();
+            string currentProcessName = currentProcess.ProcessName;
+            Mutex mutex = new Mutex(true, currentProcessName, out isRun);
+            return isRun;
+        }
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            if (program.isAlreadyRun())
+            {
+                Console.WriteLine("NOT Already Run!");
+            }
+            else
+            {
+                Console.WriteLine("Already Run!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
